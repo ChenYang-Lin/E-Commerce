c10819e6cca1c9158c8c2ee6ce8e29a159aa9572
DESKTOP-64UVUHA\cheny <chenyanglinq@gmail.com>
2021-12-07 23:29:35 -0500

    purchase history, admin add product

--- Finalproject/settings.py
+++ Finalproject/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['static']
-# STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = ['static']
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT =  BASE_DIR / 'static/images'
 MEDIA_URL = '/images/'
